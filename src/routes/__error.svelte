07437b65f358a6605640f9519bc2b2a6852279b6
tofu345 <shammahayotunde1@gmail.com>
2022-09-05 13:19:31 +0100

    initial commit

---
+++ src/routes/__error.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  export function load({ error }) {
+    return {
+      props: { message: error.message },
+    };
+  }
+</script>
+
+<script>
+  export let message;
+</script>
+
+<div class="error">
+  <h2>Ooops</h2>
+  <p>{message}</p>
+  <a href="/guides">View All Guides...</a>
+</div>
+
+<style>
+  .error {
+    text-align: center;
+    margin-bottom: 60px;
+  }
+</style>
